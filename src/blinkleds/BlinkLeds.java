771241a8f2eb723b077ec5d5ae04f4e0da0b24b6
Adrianna Cencora <adrianna.cencora@gmail.com>
2019-04-27 23:54:01 +0200

    Deal with serial port communication

--- src/blinkleds/BlinkLeds.java
+++ src/blinkleds/BlinkLeds.java
@@ -1,71 +1,17 @@
 package blinkleds;
-import gnu.io.*;
-import java.io.IOException;
-
-import java.io.OutputStream;
-import java.util.Enumeration;
-
 
 public class BlinkLeds {
-
-    private SerialPort serial;
-    private OutputStream output;
-
-    public static final int DATA_RATE = 9600;
-    public static final int TIMEOUT = 600;
-
     public static final int LIGHT_ON = 0x01;
-    public static final int LIGHT_OFF = 0x02;
+    public static final int LIGHT_OFF = 0x00;
     
-    
-    private void initSerial() {
-    CommPortIdentifier serialPortId = null;
-    Enumeration enumComm = CommPortIdentifier.getPortIdentifiers();
-
-    while (enumComm.hasMoreElements() && serialPortId == null) {
-        serialPortId = (CommPortIdentifier) enumComm.nextElement();
-    }
-
-    try {
-        serial = (SerialPort) serialPortId.open(this.getClass().getName(), TIMEOUT);
-        serial.setSerialPortParams(DATA_RATE, SerialPort.DATABITS_8,
-                                    SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-
-    } catch (PortInUseException | UnsupportedCommOperationException e) {
-        e.printStackTrace();
-    }
-}
-    private void closeSerial()  {
-        serial.close();
-    }
-    
-    
-private void initOutputStream() {
-
-    try {
-        output = serial.getOutputStream();
-    } catch (IOException e) {
-
-        e.printStackTrace();
-    }
-
-}
-
-private void sendBytes(int isSet) {
-    try { 
-        output.write(isSet);
-        
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
     public static void main(String[] args) {
-        BlinkLeds blinkleds = new BlinkLeds();
-        blinkleds.initSerial();
-        blinkleds.initOutputStream();
+        
+        //PROPER SERIAL NAME NEEDS TO BE PROVIDED BEFORE TESTING!
+        SerialPortConnectionMeneger serialConnector = new SerialPortConnectionMeneger("dev/ttysO");
+        serialConnector.initSerial();
         
         for (int i = 0; i < 10; i++) {
-            blinkleds.sendBytes(LIGHT_ON);
+            serialConnector.sendBytes(LIGHT_ON);
         
                 try {
                     Thread.sleep(1000);
@@ -74,9 +20,9 @@ private void sendBytes(int isSet) {
                     Thread.currentThread().interrupt();
                 }
                 
-            blinkleds.sendBytes(LIGHT_OFF);     
+            serialConnector.sendBytes(LIGHT_OFF);     
     }
     
-        blinkleds.closeSerial();
+        serialConnector.closeSerial();
     }
 }
