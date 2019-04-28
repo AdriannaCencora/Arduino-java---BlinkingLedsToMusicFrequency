0e760ea94652d1c36c930c96c47b435ac0aab98e
Adrianna Cencora <adrianna.cencora@gmail.com>
2019-04-28 21:58:52 +0200

    Implement handling audio player

--- src/blinkleds/AudioMeneger.java
+++ src/blinkleds/AudioMeneger.java
@@ -5,10 +5,59 @@
  */
 package blinkleds;
 
+import java.io.File;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+
+
 /**
  *
  * @author adrianna
  */
 public class AudioMeneger {
-    
+
+    private File soundFile;
+    private AudioInputStream audioStream;
+    private AudioFormat audioFormat;
+    private Clip clip;
+    private DataLine.Info info;
+
+    public void openFile(String filename) {
+        try {
+            soundFile = new File(filename);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void playAudioForTests() {
+        try {
+            audioStream = AudioSystem.getAudioInputStream(soundFile);
+            audioFormat = audioStream.getFormat();
+            info = new DataLine.Info(Clip.class, audioFormat);
+
+            clip = (Clip) AudioSystem.getLine(info);
+
+            clip.addLineListener(new LineListener() {
+                @Override
+                public void update(LineEvent event) {
+                    if (event.getType() == LineEvent.Type.STOP) {
+                        clip.close();
+                    }
+                }
+            });
+
+            clip.open(audioStream);
+            clip.start();
+        } catch (Exception exc) {
+            exc.printStackTrace(System.out);
+        }
+
+    }
 }
